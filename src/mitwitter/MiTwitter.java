28d640cadb3ca04a0aeaa59be6b63987b5b0ad0d
mmoureperez <mmoureperez@atanasoff02.danielcastelao.org>
2016-04-18 08:57:57 +0200

    Accesos

--- src/mitwitter/MiTwitter.java
+++ src/mitwitter/MiTwitter.java
@@ -8,6 +8,7 @@ package mitwitter;
 
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 /**
  *
@@ -21,6 +22,14 @@ public class MiTwitter {
     public static void main(String[] args) {
         // TODO code application logic here
         Twitter miTwitter= new TwitterFactory().getInstance();
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+    cb.setDebugEnabled(true)
+  .setOAuthConsumerKey("*********************")
+  .setOAuthConsumerSecret("******************************************")
+  .setOAuthAccessToken("**************************************************")
+  .setOAuthAccessTokenSecret("******************************************");
+TwitterFactory tf = new TwitterFactory(cb.build());
+Twitter twitter = tf.getInstance();
     }
     
 }
